9c7358fbaf2713a11813543e147cb5ce23af8290
tastyguava@gmail.com <tastyguava@gmail.com>
2020-10-02 14:46:50 +0530

    Added polybar and termite

---
+++ polybar/charging-state.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+charging_state=$(upower -i `upower -e | grep 'BAT'` | grep state | awk '{print $2}')
+
+if [ "$charging_state" = "charging" ];
+then
+    echo ''
+    sleep 0.3
+    echo  ''
+    sleep 0.3
+    echo  ''
+    sleep 0.3
+    echo  ''
+    sleep 0.3
+    echo  ''
+    sleep 0.3
+    echo  ''
+    sleep 0.3
+	echo ''
+else 
+  echo '' 
+fi
